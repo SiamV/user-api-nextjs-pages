367ca3be30577b6f6cbc7375a7e8cefd6c978dd2
SiamV <avers3452@gmail.com>
2023-07-09 21:35:17 -0500

    fix url for deploy

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
-// export const API_URL =
-//   process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
 //checks to see if its hosted on another website or localhost:3000(strapi)
 
-export const API_URL =
+export const NEXT_URL =
   process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";

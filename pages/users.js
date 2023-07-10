168683f027061baa6b3dea0bd9747f7b939388ad
SiamV <avers3452@gmail.com>
2023-07-09 23:06:03 -0500

    change css users

--- pages/users.js
+++ pages/users.js
@@ -2,24 +2,26 @@ import Link from "next/link"
 import Navbar from "../components/Navbar"
 import axios from "axios"
 import classes from "../styles/users.module.css"
-import { API_URL }  from "../config/index"
+import { API_URL } from "../config/index"
 
 const Users = ({ users }) => {
 
     return <div className={classes.usersWrapper}>
         <Navbar />
-        <p>Список пользователей</p>
-        <div>{users.map((u) =>
-            <div key={u._id} >
-                <Link href={`/users/${u._id}`}>{u.name}</Link>
-            </div>)}
+        <div className={classes.usersBlock}>
+            <p>Список пользователей</p>
+            <div>{users.map((u) =>
+                <div key={u._id} >
+                    <Link href={`/users/${u._id}`}>{u.name}</Link>
+                </div>)}
+            </div>
+            <Link href={{
+                pathname: "/users/userform",
+                query: { status: true }
+            }}>
+                <button className={classes.MenuButton}>Add user</button>
+            </Link>
         </div>
-        <Link href={{
-            pathname: "/users/userform",
-            query: { status: true }
-        }}>
-            <button className={classes.MenuButton}>Add user</button>
-        </Link>
 
     </div>
 }

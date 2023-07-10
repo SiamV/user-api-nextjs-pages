39e3827c2cabb6b849dc8af08cffe6312faa1f18
SiamV <avers3452@gmail.com>
2023-07-09 22:51:02 -0500

    change css

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/"
+  process.env.NEXT_PUBLIC_API_URL || "https://user-api-nextjs-pages.vercel.app"
 //checks to see if its hosted on another website or localhost:3000(strapi)
 
 export const NEXT_URL =
-  process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000/";
+  process.env.NEXT_PUBLIC_FRONTEND_URL || "https://user-api-nextjs-pages.vercel.app/";
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+// import { Inter } from 'next/font/google'
+// import styles from '@/styles/Home.module.css'
 import Navbar from '@/components/Navbar'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -15,7 +14,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      {/* <main className={`${styles.main} ${inter.className}`}> */}
+      <main>
         <Navbar />
         <h2>Тестовое задание</h2>
         <p>

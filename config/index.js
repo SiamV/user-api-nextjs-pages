76a10226445ce5c4ffb6524e174c9b75f82fb2fc
SiamV <avers3452@gmail.com>
2023-07-09 22:19:44 -0500

    urls

--- config/index.js
+++ config/index.js
@@ -3,4 +3,4 @@ export const API_URL =
 //checks to see if its hosted on another website or localhost:3000(strapi)
 
 export const NEXT_URL =
-  process.env.NEXT_PUBLIC_FRONTEND_URL || "http://localhost:3000";
+  process.env.NEXT_PUBLIC_FRONTEND_URL || "https://user-api-nextjs-pages.vercel.app";

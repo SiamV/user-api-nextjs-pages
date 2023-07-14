dff99698e39beece380a284aa05a5b23a9e1aaec
SiamV <avers3452@gmail.com>
2023-07-13 22:37:41 -0500

    use React for create users page for optimization upload

--- pages/users.js
+++ pages/users.js
@@ -1,47 +1,106 @@
 import Link from "next/link"
 import axios from "axios"
+import { useState, useEffect } from 'react'
 import classes from "../styles/users.module.css"
 import { API_URL } from "../config/index"
+import Preloader from "../components/common/Preloader"
 
-export default function CreateUsersList ({ usersList }) {
+
+
+export default function CreateUsersList() {
+
+    const [usersList, setUserList] = useState([])
+    const [isLoading, setLoading] = useState(false)
+
+    useEffect(() => {
+        setLoading(true)
+        const getUsersListfromDB = async () => {
+            try {
+                const getUsers = await axios.get(`${API_URL}/api/users`)
+                getUsers.status === 200 ? setLoading(false) : console.log("something wrong")
+                setUserList(getUsers.data.data.reverse())
+            } catch (e) {
+                console.log(e)
+            }
+        }
+        getUsersListfromDB()
+    }, [])
 
     return <div className={classes.usersWrapper}>
         <div className={classes.usersBlock}>
             <h3>Список пользователей</h3>
-            <div className={classes.usersListWrapper}>
-                {usersList.map((user) =>
-                    <div key={user._id} className={classes.userWrapper}>
-                        <Link href={`/users/${user._id}`}>{user.name}</Link>
-                    </div>)
-                }
-            </div>
-            <Link href={{
-                pathname: "/users/userform",
-                query: { status: true }
-            }}>
-                <button className={classes.MenuButton}>Add user</button>
-            </Link>
+            {!isLoading
+                ? <div>
+                    <div className={classes.usersListWrapper}>
+                        {usersList.map((user) =>
+                            <div key={user._id} className={classes.userWrapper}>
+                                <Link href={`/users/${user._id}`}>{user.name}</Link>
+                            </div>)
+                        }
+                    </div>
+                    <Link href={{
+                        pathname: "/users/userform",
+                        query: { status: true }
+                    }}>
+                        <button className={classes.MenuButton}>Add user</button>
+                    </Link>
+                </div>
+                : <Preloader />
+            }
+
         </div>
 
     </div>
 }
 
-export async function getStaticProps() {
-    const getUsers = await axios.get(`${API_URL}/api/users`)
 
-    return {
-      props: {
-        usersList: getUsers.data.data.reverse()
-      },
-    }
-  }
 
-// export const getServerSideProps = async () => {
 
-//         const getUsers = await axios.get(`${API_URL}/api/users`)
 
-//         return { props: { usersList: getUsers.data.data.reverse() } }
+
+
+
+// Michael, Vlad. Please leave this code. It is for exemple use SSR and SSG
+
+// export default function CreateUsersList ({ usersList }) {
+
+//     return <div className={classes.usersWrapper}>
+//         <div className={classes.usersBlock}>
+//             <h3>Список пользователей</h3>
+//             <div className={classes.usersListWrapper}>
+//                 {usersList.map((user) =>
+//                     <div key={user._id} className={classes.userWrapper}>
+//                         <Link href={`/users/${user._id}`}>{user.name}</Link>
+//                     </div>)
+//                 }
+//             </div>
+//             <Link href={{
+//                 pathname: "/users/userform",
+//                 query: { status: true }
+//             }}>
+//                 <button className={classes.MenuButton}>Add user</button>
+//             </Link>
+//         </div>
+
+//     </div>
 // }
 
+// export async function getStaticProps() {
+//     const getUsers = await axios.get(`${API_URL}/api/users`)
 
-// export default CreateUsersList
+//     return {
+//       props: {
+//         usersList: getUsers.data.data.reverse()
+//       },
+//     }
+//   }
+
+// export async function getServerSideProps() {
+//     const getUsers = await axios.get(`${API_URL}/api/users`)
+
+//     return {
+//         props: {
+//             usersList: getUsers.data.data.reverse()
+//         },
+//     }
+// }
